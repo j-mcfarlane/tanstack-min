6d882ac52b22596f0a8a017ebd73e1addf4f776a
Jason McFarlane <jmcfarlane.developer@gmail.com>
2025-06-09 16:47:18 +0400

    feat: wip

--- app/routes/__root.tsx
+++ app/routes/__root.tsx
@@ -4,8 +4,13 @@ import { QueryClient } from '@tanstack/react-query'
 // Ui
 import { RootLayout, RootLinks, RootMeta } from '@/ui/layouts'
 
+/**
+ * Here is the path issue - toggle between alias and direct import and run build to see the error
+ */
+
 // Data
 import { getSessionData } from '@/data'
+// import { getSessionData } from '../../data/session/get-session-data/get-session-data'
 
 export const Route = createRootRouteWithContext<{
     queryClient: QueryClient
