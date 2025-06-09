4789f165201bcaeb2efee277ed63b3c2f0fab58e
Jason McFarlane <jmcfarlane.developer@gmail.com>
2025-06-09 16:42:40 +0400

    feat: wip

--- app/routes/__root.tsx
+++ app/routes/__root.tsx
@@ -4,9 +4,19 @@ import { QueryClient } from '@tanstack/react-query'
 // Ui
 import { RootLayout, RootLinks, RootMeta } from '@/ui/layouts'
 
+// Data
+import { getSessionData } from '@/data'
+
 export const Route = createRootRouteWithContext<{
     queryClient: QueryClient
 }>()({
+    beforeLoad: async ({ context }) => {
+        const data = await getSessionData()
+
+        return {
+            ...context,
+        }
+    },
     head: () => ({
         meta: RootMeta,
         links: RootLinks,
